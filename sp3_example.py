658af8c1e31d88d59906cea86fdc029dd1d4c012
Henni <henningmartin.woydt@uni-jena.de>
2022-10-24 19:01:12 +0200

    * reformat doc

--- sp3_example.py
+++ sp3_example.py
@@ -27,11 +27,11 @@ def get_match():
 # If we want to spawn new processes, we have to make sure our program starts in a proper Python entry point.
 if __name__ == "__main__":
     """
-        Now all we have to do is make an instance of the SB3MultipleInstanceEnv and pass it our get_match function, the number of instances we'd like to open, and how long it should wait between instances.
-        This wait_time argument is important because if multiple Rocket League clients are opened in quick succession, they will cause each other to crash. The exact reason this happens is unknown to us,
-        but the easiest solution is to delay for some period of time between launching clients. The amount of required delay will depend on your hardware, so make sure to change this number if your Rocket League
-        clients are crashing before they fully launch.
+    Now all we have to do is make an instance of the SB3MultipleInstanceEnv and pass it our get_match function, the number of instances we'd like to open, and how long it should wait between instances.
+    This wait_time argument is important because if multiple Rocket League clients are opened in quick succession, they will cause each other to crash. The exact reason this happens is unknown to us,
+    but the easiest solution is to delay for some period of time between launching clients. The amount of required delay will depend on your hardware, so make sure to change this number if your Rocket League
+    clients are crashing before they fully launch.
     """
-    env = SB3MultipleInstanceEnv(match_func_or_matches=get_match, num_instances=2, wait_time=20)
+    env = SB3MultipleInstanceEnv(match_func_or_matches=get_match, num_instances=4, wait_time=20)
     learner = PPO(policy="MlpPolicy", env=env, verbose=1)
     learner.learn(1_000_000)
