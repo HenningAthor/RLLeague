55f24bb5e12cd9d9868cfc9b88380fe7e7d247bd
Henning <henningmartin.woydt@uni-jena.de>
2023-01-17 19:35:14 +0100

    * root of a tree is no a DecisionNode

--- genetic_lab/bot_generation.py
+++ genetic_lab/bot_generation.py
@@ -26,6 +26,7 @@ def create_bot(bot_id: int, min_depth: int, max_depth: int, env_vars: Dict) -> B
     bot.jump_root = create_tree(min_depth, max_depth, env_vars)
     bot.boost_root = create_tree(min_depth, max_depth, env_vars)
     bot.handbrake_root = create_tree(min_depth, max_depth, env_vars)
+    bot.creation_variables = env_vars
     return bot
 
 
@@ -33,25 +34,13 @@ def create_tree(min_depth: int, max_depth: int, env_vars: Dict) -> Node:
     """
     Creates a tree after a formal grammar. After min_depth the probability a
     leaf will occur rises, until it is at 100% at max_depth. The root is at
-    depth 0. Root will always be a DecisionNode with 'kickoff' as Bool
-    variable.
+    depth 0.
 
     :param min_depth: Minimal depth of the trees.
     :param max_depth: Maximal depth of the trees.
     :param env_vars: Dict of variables present in the environment.
     :return: Root of the tree.
     """
-    kickoff_node = LogicParameterNode()
-    kickoff_node.parameter = 'kickoff'
-
-    c1_node = np.random.choice(all_nodes['BRANCH']['ARITHMETIC'])()
-    c2_node = np.random.choice(all_nodes['BRANCH']['ARITHMETIC'])()
-
-    c1_node.construct_tree(1, min_depth, max_depth, env_vars)
-    c2_node.construct_tree(1, min_depth, max_depth, env_vars)
-
     root = DecisionNode()
-    root.add_child(kickoff_node)
-    root.add_child(c1_node)
-    root.add_child(c2_node)
+    root.construct_tree(1, min_depth, max_depth, env_vars)
     return root
