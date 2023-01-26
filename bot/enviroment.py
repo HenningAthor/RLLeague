254e771d977cc9b47ea9f0085ecfb376a9126f15
Henning <henningmartin.woydt@uni-jena.de>
2023-01-26 08:57:52 +0100

    * removed old files

--- bot/enviroment.py
+++
@@ -1,7 +0,0 @@
-def get_environment_stats():
-    """
-    Reads the data files to determine the minimum and maximum value for all
-    parameters.
-
-    :return: Dictionary holding all parameters and their values.
-    """
