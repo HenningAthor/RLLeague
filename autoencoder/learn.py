3f0672ecabe9d40622cc612b5851a61e9e5e96c5
BMI24 <mitgliedschaftfo@gmx.de>
2023-02-06 13:31:09 +0100

    add first draft of autoencoder

--- autoencoder/learn.py
+++ autoencoder/learn.py
@@ -8,6 +8,9 @@ from torch.utils.data import IterableDataset
 import glob
 import pathlib
 import pandas as pd
+from typing import List
+from itertools import product
+import numpy as np
 
 """
 Implementation from: https://medium.com/pytorch/implementing-an-autoencoder-in-pytorch-19baa22647d1
@@ -30,20 +33,64 @@ class AE(nn.Module):
         reconstructed = torch.relu(activation)
         return reconstructed
 
-class RocketLeagueDataset(IterableDataset):
-    INPUT_SIZE = 10
-    def __init__(self, path : str):
-        self.path = path
+def string_product(separator : str, *lists : List[str]):
+    return [separator.join(x) for x in product(*lists)]
 
-    def extract_information(row : pd.Series):
-        raise NotImplementedError()
+class RocketLeagueDataset(IterableDataset):
+    INPUT_SIZE = 18
+    """    env_variables = {'ARITHMETIC': ['inverted_ball/pos_x',
+                                    'inverted_ball/pos_y',
+                                    'inverted_ball/pos_z',
+                                    'inverted_ball/vel_x',
+                                    'inverted_ball/vel_y',
+                                    'inverted_ball/vel_z',
+                                    'player1/pos_x',
+                                    'player1/pos_y',
+                                    'player1/pos_z',
+                                    'player1/vel_x',
+                                    'player1/vel_y',
+                                    'player1/vel_z',
+                                    'inverted_player2/pos_x',
+                                    'inverted_player2/pos_y',
+                                    'inverted_player2/pos_z',
+                                    'inverted_player2/vel_x',
+                                    'inverted_player2/vel_y',
+                                    'inverted_player2/vel_z'] """
+    def __init__(self, path : str, half_count = 100000):
+        self.path = path
+        self.half_count = half_count
+        self.x = 0
+
+    def extract_information(self, row : pd.Series, ids : List[str], inverted : bool):
+        player1 = 'inverted_' + ids[1] if inverted else ids[0]
+        player2 = 'inverted_' + ids[0] if inverted else ids[1]
+        ball = 'inverted_ball' if inverted else 'ball'
+
+        data_of_interest1 = [player1, player2, ball]
+        data_of_interest2 = ['/pos_x', '/pos_y', '/pos_z', '/vel_x', '/vel_y', '/vel_z']
+
+        return row[string_product('', data_of_interest1, data_of_interest2)].to_numpy(dtype=np.float32)
+        
+
     
     def __iter__(self):
-        paths = glob.glob(pathlib.Path(self.path) / '*.parquet')
+        paths = glob.glob(self.path + '/*.parquet')
+
+        i = 0
+        
         for path in paths:
             df = pd.read_parquet(path)
+            ids = [name_column[:-len('/name')] for name_column in df.columns[df.columns.str.contains('/name')]]
             for index, row in df.iterrows():
-                yield row
+                i += 1
+                if i > self.half_count:
+                    return
+
+                yield self.extract_information(row, ids, False)
+                yield self.extract_information(row, ids, True)
+    
+    def __len__(self):
+        return 2 * self.half_count
 
 
 def learn_mnist():
@@ -101,15 +148,14 @@ def learn_mnist():
         # display the epoch training loss
         print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))
 
-def learn_rl(path : str, epochs: int):
-    epochs = 100
+def learn_rl(path : str = 'ae_data', epochs: int = 1000):
 
     #  use gpu if available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # create a model from `AE` autoencoder class
     # load it to the specified device, either gpu or cpu
-    model = AE(input_size= RocketLeagueDataset.INPUT_SIZE, hidden_size=32).to(device)
+    model = AE(input_size= RocketLeagueDataset.INPUT_SIZE, hidden_size=5).to(device)
 
     # create an optimizer object
     # Adam optimizer with learning rate 1e-3
@@ -119,12 +165,12 @@ def learn_rl(path : str, epochs: int):
     criterion = nn.MSELoss()
 
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-    train_dataset = RocketLeagueDataset('ae_data')
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True)
+    train_dataset = RocketLeagueDataset(path)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, num_workers=1, pin_memory=True)
 
     for epoch in range(epochs):
         loss = 0
-        for batch_features, _ in train_loader:
+        for batch_features in train_loader:
             # reshape mini-batch data to [N, 784] matrix
             # load it to the active device
             batch_features = batch_features.view(-1, RocketLeagueDataset.INPUT_SIZE).to(device)
--- autoencoder_main.py
+++ autoencoder_main.py
@@ -1,8 +1,9 @@
 """
 Script to learn an autoencoder.
 """
-from autoencoder.learn import learn_mnist
+from autoencoder.learn import learn_rl, learn_mnist
 
 
 if __name__ == '__main__':
-    learn_mnist()
+    learn_rl()
+
